d54cf89a83f2f50b35a3d56427b0e90627681568
minoiu <nicoleminoiu@gmail.com>
2017-06-05 17:02:55 +0100

    creating profile sliding menu

---
+++ app/src/main/java/com/qmul/nminoiu/tunein/Fragment/Fragment1.java
@@ -0,0 +1,26 @@
+package com.qmul.nminoiu.tunein.Fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.qmul.nminoiu.tunein.R;
+
+public class Fragment1 extends Fragment {
+    public Fragment1() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View rootView = inflater.inflate(R.layout.fragment1, container, false);
+        return rootView;
+    }
+
+
+}
---
+++ app/src/main/java/com/qmul/nminoiu/tunein/model/ItemSlideMenu.java
@@ -0,0 +1,32 @@
+package com.qmul.nminoiu.tunein.model;
+
+/**
+ * Created by nminoiu on 05/06/2017.
+ */
+
+public class ItemSlideMenu {
+
+    private int imgId;
+    private String title;
+
+    public ItemSlideMenu(int imgId, String title) {
+        this.imgId = imgId;
+        this.title = title;
+    }
+
+    public int getImgId() {
+        return imgId;
+    }
+
+    public void setImgId(int imgId) {
+        this.imgId = imgId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
