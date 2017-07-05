5cd7d26d2fb6572c6140b1e31c2dba558e859f1a
minoiu <nicoleminoiu@gmail.com>
2017-07-05 17:12:01 +0100

    designing now playing

---
+++ app/src/main/java/com/qmul/nminoiu/tunein/NowPlayingItem.java
@@ -0,0 +1,83 @@
+package com.qmul.nminoiu.tunein;
+
+/**
+ * Created by nminoiu on 05/07/2017.
+ */
+
+public class NowPlayingItem {
+    private int id;
+    private String name, status, image, profilePic, timeStamp, url;
+
+    public NowPlayingItem(){
+    }
+
+    public NowPlayingItem(int id, String name, String image, String status,
+                          String profilePic, String timeStamp, String url){
+        super();
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.status = status;
+        this.profilePic = profilePic;
+        this.timeStamp = timeStamp;
+        this.url = url;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImge() {
+        return image;
+    }
+
+    public void setImge(String image) {
+        this.image = image;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getProfilePic() {
+        return profilePic;
+    }
+
+    public void setProfilePic(String profilePic) {
+        this.profilePic = profilePic;
+    }
+
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
+
+
