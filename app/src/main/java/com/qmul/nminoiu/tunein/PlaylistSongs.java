254ce2e26440b50d5ab2fe96edf9e02c66ff2b86
minoiu <nicoleminoiu@gmail.com>
2017-08-22 16:57:26 +0100

    fixing menus

--- app/src/main/java/com/qmul/nminoiu/tunein/PlaylistSongs.java
+++ app/src/main/java/com/qmul/nminoiu/tunein/PlaylistSongs.java
@@ -53,8 +53,8 @@ public class PlaylistSongs extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent i = new Intent(PlaylistSongs.this, Users.class);
+                startActivity(i);
             }
         });
 
