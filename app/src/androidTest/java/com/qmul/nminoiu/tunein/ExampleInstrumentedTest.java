80cf9cb5bf202ddc591ba4367d22bc1c9b5e1a94
nicoleta <nicoleminoiu@gmail.com>
2018-03-12 15:58:10 +0000

    fixing dependencies after update

--- app/src/androidTest/java/com/qmul/nminoiu/tunein/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/qmul/nminoiu/tunein/ExampleInstrumentedTest.java
@@ -2,11 +2,21 @@ package com.qmul.nminoiu.tunein;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 import static org.junit.Assert.*;
 
 /**
@@ -16,11 +26,23 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityTestRule<LogoActivity> mActivityTestRule = new ActivityTestRule<>(LogoActivity.class);
+
+
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.qmul.nminoiu.tunein", appContext.getPackageName());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.email_sign_in_button), withText("Tune In!"),
+                        withParent(allOf(withId(R.id.layoutlogin),
+                                withParent(withId(R.id.login_form))))));
+        appCompatButton.perform(scrollTo(), click());
+
     }
 }
